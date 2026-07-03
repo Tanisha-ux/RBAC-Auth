dbfc0dee6be89a8360b6b8d16ece717a5f7447b9
Tanisha Negi <negitanisha77@gmail.com>
2026-07-03 18:52:49 +0530

    Updated DB pool to Neon connection

--- Desktop/Auth-system/models/db.js
+++ Desktop/Auth-system/models/db.js
@@ -1,12 +1,23 @@
 
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "authdb",
-  password: "tani@2005",
-  port: 5432,
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "authdb",
+//   password: "tani@2005",
+//   port: 5432,
+// });
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
