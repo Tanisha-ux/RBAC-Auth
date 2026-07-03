f580c9a1b94d0cd01b9a1ee02c8ba3fed87ee654
Tanisha Negi <negitanisha77@gmail.com>
2026-07-03 19:05:35 +0530

    fix render port binding issue

--- Desktop/Auth-system/index.js
+++ Desktop/Auth-system/index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-const PORT=3001;
+const PORT=process.env.PORT || 3001;
 
 const cookieParser=require("cookie-parser");
 const { restrictToLoggedInUser } = require("./middlewares/auth.js"); 
